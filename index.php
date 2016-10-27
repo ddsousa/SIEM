d420cef1e262eae53e6ff8d97c1b2cdeb9ad1a61
pmrelvas <pedromiguelrelvas@gmail.com>
2016-10-27 10:26:41 +0100

    home page estática semi-feita

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 	<title>Oceano Hipermercado</title>
 	<link rel="icon" href="media/icon.png">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
 </head>
 <body>
 	<div id="wrapper">
@@ -11,7 +12,101 @@
 			<img src="media/logo.png" class="logo">
 		</div>
 		<div id="container">
-			
+			<h4 class="titulo-centrado">Produtos mais vendidos</h4>
+			<div>
+				<table class="tab-centrada">
+					<tr> <!-- imagens produto -->
+						<td>
+							<img class="img-produto" src="media/temp.jpg" alt="fotografia produto"> <!-- Isto aqui era fixe que a tabela das imagens tivesse uma descricao do que e' o produto para meter no campo alt ;)-->
+						</td>
+						<td>
+							<img class="img-produto" src="media/temp.jpg" alt="fotografia produto"> <!-- Isto aqui era fixe que a tabela das imagens tivesse uma descricao do que e' o produto para meter no campo alt ;)-->
+						</td>
+						<td>
+							<img class="img-produto" src="media/temp.jpg" alt="fotografia produto"> <!-- Isto aqui era fixe que a tabela das imagens tivesse uma descricao do que e' o produto para meter no campo alt ;)-->
+						</td>
+						<td>
+							<img class="img-produto" src="media/temp.jpg" alt="fotografia produto"> <!-- Isto aqui era fixe que a tabela das imagens tivesse uma descricao do que e' o produto para meter no campo alt ;)-->
+						</td>
+					</tr>
+					<tr> <!-- nome e preco produto -->
+						<td>
+							<table class="tab-centrada">
+								<tr>
+									<td>David</td>
+									<td>1.00€</td>
+								</tr>
+							</table>
+						</td>
+						<td>
+							<table class="tab-centrada">
+								<tr>
+									<td>David</td>
+									<td>1.00€</td>
+								</tr>
+							</table>
+						</td>
+						<td>
+							<table class="tab-centrada">
+								<tr>
+									<td>David</td>
+									<td>1.00€</td>
+								</tr>
+							</table>
+						</td>
+						<td>
+							<table class="tab-centrada">
+								<tr>
+									<td>David</td>
+									<td>1.00€</td>
+								</tr>
+							</table>
+						</td>
+					</tr>
+				</table>
+			</div>
+			<hr>
+			<div style="margin: 3% auto">
+				<table class="tab-centrada">
+					<tr>
+						<td>
+							<img class="membro-equipa" src="media/foto_david.png" alt="Fotografia de David de Sousa">
+						</td>
+						<td>
+							<img class="membro-equipa" src="media/foto_relvas.jpg" alt="Fotografia de Pedro Relvas">
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<a href="https://sigarra.up.pt/feup/pt/fest_geral.cursos_list?pv_num_unico=201203863" target="_blank">David de Sousa</a>
+						</td>
+						<td>
+							<a href="https://sigarra.up.pt/feup/pt/fest_geral.cursos_list?pv_num_unico=201205118" target="_blank">Pedro Relvas</a>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<a href="mailto:ee12183@fe.up.pt">ee12183@fe.up.pt</a>
+						</td>
+						<td>
+							<a href="mailto:ee12036@fe.up.pt">ee12036@fe.up.pt</a>
+						</td>
+					</tr>
+				</table>
+			</div>
+			<div>
+				<h4 class="titulo-centrado">Versão Provisória</h4>
+				<table class="tab-centrada-peq">
+					<tr>
+						<td>usersiem</td>
+						<td>pass:qwerty</td>
+					</tr>
+					<tr>
+						<td>adminsiem</td>
+						<td>pass:12345</td>
+					</tr>
+				</table>
+			</div>
 		</div>
 		<div id="footer" class="menu">
 			<ul class="centered">
