80cc24420addf096167ed6f1b30c4179229287fe
pmrelvas <pedromiguelrelvas@gmail.com>
2016-10-27 09:13:26 +0100

    adicao de header e footer na pagina incial

--- index.php
+++ index.php
@@ -1,10 +1,24 @@
-<!DOCTYPE html>
 <html>
 <head>
 	<meta charset="UTF-8">
 	<title>Oceano Hipermercado</title>
+	<link rel="icon" href="media/icon.png">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
-	<h1>Oceano Hipermercados</h1>
+	<div id="wrapper">
+		<div id="header">
+			<img src="media/logo.png" class="logo">
+		</div>
+		<div id="container">
+			
+		</div>
+		<div id="footer" class="menu">
+			<ul class="centered">
+				<li class="with-separator"><a href=#>Contactos</a></li>
+				<li><a href=#>Relatório</a></li>
+			</ul>
+		</div>
+	</div>
 </body>
 </html>
