b69cdc7387d36cc04d3943a4da546b2e903a51ea
pmrelvas <pedromiguelrelvas@gmail.com>
2016-10-29 21:50:12 +0100

    merge

--- common/init.php
+++ common/init.php
@@ -1,6 +1,10 @@
 <?php
   session_start();
 
+	if(empty($_SESSION['cart_item'])) {
+	  $_SESSION['cart_item'] = array();
+	}
+
   $BASE_DIR = dirname(__DIR__);
 
   $conn = pg_connect("host=db.fe.up.pt dbname=siem1636 user=siem1636 password=siempr16");
--- pages/other/displayProduto.php
+++ pages/other/displayProduto.php
@@ -3,6 +3,55 @@
 	include_once("../../common/header.php");
 	include_once("../../common/navbar.php");
 
+	if(!empty($_GET['action'])) {
+		switch($_GET['action']) {
+			case 'add':
+				if(!empty($_POST['quantity'])) {
+					$query = "SELECT *
+										FROM produto
+										WHERE id =".$_GET['id'].";";
+					$result = pg_exec($conn, $query);
+					$product = pg_fetch_row($result, 0);
+
+					$cart_item = array($product[0]=>array('id'=>$product[0], 'name'=>$product[2], 'price'=>$product[5], 'quantity'=>$_POST['quantity']));
+
+					echo '<br>'.$product[0].'<br>';
+					echo var_dump($_SESSION['cart_item']).'<br>';
+					if(!empty($_SESSION['cart_item'])) {
+						if(array_key_exists($product[0], $_SESSION['cart_item'])) {
+							foreach($_SESSION['cart_item'] as $i => $j) {
+								if($product[0] == $i) {
+									$_SESSION['cart_item'][$i]['quantity'] += $_POST['quantity'];
+								}
+							}
+						} else {
+							echo 'nao tinha este id';
+							$_SESSION['cart_item'] = array_merge($_SESSION['cart_item'], $cart_item);
+						}
+					} else {
+						echo 'estava vazio';
+						$_SESSION['cart_item'] = $cart_item;
+					}
+				}
+			break;
+			case 'remove':
+				if(!empty($_SESSION['cart_item'])) {
+					foreach($_SESSION['cart_item'] as $i => $j) {
+						if($_GET['id'] == $i) {
+							unset($_SESSION['cart_item'][$i]);
+						}
+						if(empty($_SESSION['cart_item'])) {
+							unset($_SESSION['cart_item']);
+						}
+					}
+				}
+			break;
+			case 'empty':
+				unset($_SESSION['cart_item']);
+			break;
+		}
+	}
+
 	$prod_id = $_GET['id'];
 
 	$query = "SELECT *
@@ -28,12 +77,30 @@
 				?>
 			</div>
 			<div class="order">
-				<form action="">
+				<form method="POST" action="displayProduto.php?action=add&id=<?php echo $prod_id;?> ">
 					<input style="width: 40px;" class="order-item" type="text" name="quantity" value="1">
 					Unid.
 					<input type="submit" value="+Adicionar ao carrinho" class="btn-princ order-item">
 				</form>
 			</div>
+		</div>
+
+		<!-- DEBUG TODO: apagar-->
+		<div>
+			<h3>DEBUG: Carrinho</h3>
+			<table>
+			<?php
+				foreach($_SESSION['cart_item'] as $item) {
+			?>
+				<tr>
+					<td>id: <?php echo $item['id']; ?></td>
+					<td>nome: <?php echo $item['name']; ?></td>
+					<td>quantidade: <?php echo $item['quantity']; ?></td>
+				</tr>
+			<?php
+				}
+			?>
+			</table>
 		</div>
 	</div>
 <?php
