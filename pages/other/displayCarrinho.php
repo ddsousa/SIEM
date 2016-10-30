3619af70f4351fd6ccd11c89ff4f1e92fe49d2d1
pmrelvas <pedromiguelrelvas@gmail.com>
2016-10-30 23:16:41 +0000

    adicao de funcionalidade para editar quantidade de um produto no carrinho

--- pages/other/displayCarrinho.php
+++ pages/other/displayCarrinho.php
@@ -6,13 +6,26 @@
 	if(!empty($_GET['action'])) {
 		switch($_GET['action']) {
 			case 'remove':
-				if(!empty($_GET['id'])) {
+				if(!empty($_GET['id']) && !empty($_POST['quantity'])) {
 					$id = $_GET['id'];
 					if(!empty($_SESSION['cart_item'])) {
 						foreach($_SESSION['cart_item'] as $item) {
 							if($item['id'] == $id) {
 								$key = array_search($item, $_SESSION['cart_item']);
 								unset($_SESSION['cart_item'][$key]);
+							}
+						}
+					}
+				}
+			break;
+			case 'edit':
+				if(!empty($_GET['id'])) {
+					$id = $_GET['id'];
+					if(!empty($_SESSION['cart_item'])) {
+						foreach ($_SESSION['cart_item'] as $item) {
+							if($item['id'] == $id) {
+								$key = array_search($item, $_SESSION['cart_item']);
+								$_SESSION['cart_item'][$key]['quantity'] = $_POST['quantity'];
 							}
 						}
 					}
@@ -34,6 +47,7 @@
 	<div class="display-prod">
 		<h3>Carrinho</h3>
 		<table>
+
 			<?php
 				foreach($_SESSION['cart_item'] as $item) {
 			?>
@@ -45,14 +59,17 @@
 					<?php echo $item['name'].'<br>'.$item['price']; ?>
 				</td>
 				<td>
-					<?php echo $item['quantity'];?>
+					<form method="POST" action="displayCarrinho.php?action=edit&id=<?php echo $item['id']?>">
+						<input type="text" class="text-input-quantity" value="<?php echo $item['quantity'];?>" name="quantity" >
+						<input type="submit" value="Editar" class="btn-princ btn-large">
+					</form>
 				</td>
 				<td>
 					<?php echo $item['price'];?>
 				</td>
 				<td>
 					<form method="POST" action="displayCarrinho.php?action=remove&id=<?php echo $item['id']; ?>">
-						<input type="submit" value="Remover" class="btn-princ">
+						<input type="submit" value="Remover" class="btn-princ btn-large">
 					</form>
 				</td>
 			</tr>
@@ -63,10 +80,10 @@
 		</table>
 
 		<form method="POST" action="displayCarrinho.php?action=clean">
-			<input type="submit" value="Limpar" class="btn-princ">
+			<input type="submit" value="Limpar" class="btn-princ btn-large">
 		</form>
 		<form method="POST" action="displayCarrinho.php?action=checkout">
-			<input type="submit" value="Finalizar Encomenda" class="btn-princ">
+			<input type="submit" value="Finalizar Encomenda" class="btn-princ btn-large">
 		</form>
 	</div>
 </div>
