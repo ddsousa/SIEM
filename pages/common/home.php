427153781072c8aed6ae1b590be71bb2880fafcd
pmrelvas <pedromiguelrelvas@gmail.com>
2016-10-30 11:51:31 +0000

    Display de todos os produtos (falta dividir em paginas e categorias)

--- pages/common/home.php
+++ pages/common/home.php
@@ -6,41 +6,9 @@
 	 </div>
 	 </html>
 		<div id="container">
-			<h4 class="titulo-centrado">Produtos mais vendidos</h4>
-			<div>
-				<table class="tab-centrada">
-					<tr>
-						<?php
-							$query = "SELECT id, nome, preco
-									  FROM produto
-									  ORDER BY n_vendas DESC, nome";
-							$result = pg_exec($conn, $query);
-
-							for($i=0; $i<4; $i++) { // imagens produto
-								$row = pg_fetch_row($result, $i);
-								echo '<td>';
-								echo 	'<img class="img-produto" src="../../media/img/products/'.$row[0].'.jpg" alt="fotografia de '.$row[1].'">';
-								echo '</td>';
-							}
-
-							echo '</tr>';
-							echo '<tr>';
-
-							for($i=0; $i<4; $i++) { // nome e preco
-								$row = pg_fetch_row($result, $i);
-								echo '<td>';
-								echo 	'<table class="tab-centrada texto-produtos">';
-								//echo 		'<td style="text-align: left;">1</td>';
-								//echo 		'<td style="text-align: right;">2</td>';
-								echo 		'<td style="text-align: left;"><a href="../../pages/other/displayProduto.php?id='.$row[0].'">'.$row[1].'</a></td>';
-								echo 		'<td style="text-align: right;">'.$row[2].'€</td>';
-								echo 	'</table>';
-								echo '</td>';
-							}
-						?>
-					</tr>
-				</table>
-			</div>
+			<?php
+				include_once("../../common/display_mais_vendidos.php");
+			?>
 			<hr>
 			<div style="margin: 3% auto">
 				<table class="tab-centrada">
--- pages/other/displayProdutos.php
+++ pages/other/displayProdutos.php
@@ -2,11 +2,49 @@
 	include_once("../../common/init.php");
 	include_once("../../common/header.php");
 	include_once("../../common/navbar.php");
+
+	$query = "SELECT id, nome, preco
+						FROM produto";
+	$prod_array = pg_exec($conn, $query);
 ?>
 
 </div>
 <div id="container">
-	
+		<?php
+			include_once("../../common/display_mais_vendidos.php");
+		?>
+		<hr>
+		<h4 class="titulo-centrado">Todos os produtos</h4>
+		<table class="tab-centrada">
+				<?php
+					$n_rows = pg_num_rows($prod_array);
+					for($j=0; $j<$n_rows; $j+=4) {
+						echo '<tr>';
+						for($i=0; $i<4; $i++) {	// imagens produtos
+							echo '<td>';
+							if(($i+$j)<$n_rows) {
+								$prod = pg_fetch_row($prod_array, $i+$j);
+								echo 	'<img class="img-produto" src="../../media/img/products/'.$prod[0].'.jpg" alt="fotografia de '.$prod[1].'">';
+							}
+							echo '</td>';
+						}
+						echo '</tr>';
+						echo '<tr>';
+						for($i=0; $i<4; $i++) { // nome e preco
+							echo '<td>';
+							if(($i+$j)<$n_rows) {
+								$prod = pg_fetch_row($prod_array, $i+$j);
+								echo 	'<table class="tab-centrada texto-produtos">';
+								echo 		'<td style="text-align: left;"><a href="../../pages/other/displayProduto.php?id='.$prod[0].'">'.$prod[1].'</a></td>';
+								echo 		'<td style="text-align: right;">'.$prod[2].'€</td>';
+								echo 	'</table>';
+							}
+							echo '</td>';
+						}
+						echo '</tr>';
+					}
+				?>	
+	</table>
 </div>
 
 <?php
