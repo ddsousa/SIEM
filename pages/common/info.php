5529f704335c351ff1e26c014b9a3f3eab5d965b
pmrelvas <pedromiguelrelvas@gmail.com>
2016-11-01 18:41:19 +0000

    Revert "Merged branch master into master"
    This reverts commit 6d3d77614d7c843474b06d89026c82a66335d4a7.

--- pages/common/info.php
+++
@@ -1,7 +0,0 @@
-<?php
-  include_once('../../common/init.php');
-  include_once ($BASE_DIR . "/common/header.php");
-  include_once ($BASE_DIR . "/common/navbar.php");
-  include_once ($BASE_DIR . "/common/messages.php");
-  include_once ($BASE_DIR . "/common/footer.php");
-?>
