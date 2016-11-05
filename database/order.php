6fe8f11fe9eb05f4da7902cc2a0187e60b86c6c1
daviddesousa <dmrsousa@sapo.pt>
2016-11-05 16:33:34 +0000

    orders sort by.

--- database/order.php
+++ database/order.php
@@ -17,4 +17,27 @@
     return $result;
   }
 
+  function getAllOrders($sort_by) {
+    global $conn;
+
+    $add_query = "";
+
+    if($sort_by!=null) {
+      $add_query = " ORDER BY $sort_by";
+    }
+
+    $result = pg_query($conn, "SELECT cliente.nome, numero, estado, data_efetuada, COUNT(encomenda.numero) AS artigos, SUM(quantidade * preco) AS total
+                               FROM encomenda
+                               JOIN cliente ON encomenda.id_cliente = cliente.id
+                               JOIN detalhesencomenda ON detalhesencomenda.id_encomeda = encomenda.id
+                               JOIN produto ON detalhesencomenda.id_produto = produto.id
+                               GROUP BY encomenda.id, cliente.id" . $add_query);
+    if (!$result) {
+      echo "An error occured.\n";
+      exit;
+    }
+
+    return $result;
+  }
+
  ?>
