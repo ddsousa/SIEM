922664790579f82f99f7246f7abcd728ee40fc41
daviddesousa <dmrsousa@sapo.pt>
2016-11-08 21:23:39 +0000

    Report page.

---
+++ pages/other/report.php
@@ -0,0 +1,25 @@
+<?php
+  include_once('../../common/init.php');
+  include_once ($BASE_DIR . "/common/header.php");
+  include_once ($BASE_DIR . "/common/navbar.php");
+  include_once ($BASE_DIR . "/common/messages.php");
+?>
+
+  <div id="container">
+
+    <h3>Relatório</h3>
+
+    <p><br>Este trabalho foi desenvolvido no âmbito da unidade curricula de Sistemas de Informação Empresariais.<br>
+        Abaixo poderá descarregar os seguintes recursos.<br><br></p>
+
+        <table>
+          <tr><td><a href="#"><input style="width: 150px;" type="button" value="PPT"></input></a></td></tr>
+          <tr><td><a href="#"><input style="width: 150px;" type="button" value="ZIP"></input></a></td></tr>
+          <tr><td><a href="#"><input style="width: 150px;" type="button" value="CSS"></input></a></td></tr>
+        </table>
+
+  </div>
+
+<?php
+  include_once ($BASE_DIR . "/common/footer.php");
+?>
