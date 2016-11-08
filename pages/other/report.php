44264e2dd4032ae12d35716f6b38cdaaceb966a5
pmrelvas <pedromiguelrelvas@gmail.com>
2016-11-08 22:41:37 +0000

    report design update

--- pages/other/report.php
+++ pages/other/report.php
@@ -7,17 +7,24 @@
 
   <div id="container">
 
-    <h3>Relatório</h3>
+    <h3 class="page-title">Relatório</h3>
 
     <p><br>Este trabalho foi desenvolvido no âmbito da unidade curricula de Sistemas de Informação Empresariais.<br>
         Abaixo poderá descarregar os seguintes recursos.<br><br></p>
 
-        <table>
+       <!-- <table>
           <tr><td><a href="#"><input style="width: 150px;" type="button" value="PPT"></input></a></td></tr>
           <tr><td><a href="#"><input style="width: 150px;" type="button" value="ZIP"></input></a></td></tr>
           <tr><td><a href="#"><input style="width: 150px;" type="button" value="CSS"></input></a></td></tr>
-        </table>
-
+        </table> -->
+        
+        <table class="tab-icons tab-centrada">
+            <tr>
+              <td><a href="#"><img class="icon" src="../../media/img/icons/icon_powerpoint.png" alt="Logótipo Microsoft PowerPoint"></a></td>
+              <td><a href="#"><img class="icon" src="../../media/img/icons/icon_css.png" alt="Logótipo CSS3"></a></td>
+              <td><a href="#"><img class="icon" src="../../media/img/icons/icon_zip.png" alt="Icon de ZIP"></a></td>
+            </tr>
+          </table>
   </div>
 
 <?php
