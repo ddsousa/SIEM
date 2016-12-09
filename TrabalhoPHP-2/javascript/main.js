c440eeb2ba38c386628ac3ea9989f36c18fb728f
Pedro Relvas <pedromiguelrelvas@gmail.com>
2016-12-09 09:48:07 +0000

    subnavbar (product types) active style added

--- TrabalhoPHP-2/javascript/main.js
+++ TrabalhoPHP-2/javascript/main.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
-
+	// add navbar active class
 	var current = window.location.href.substring(window.location.href.lastIndexOf('/')+1, window.location.href.lastIndexOf('.'));
 	$('#nav_' + current).addClass('active');
+
+	//add subnavbar active class (only on products page)
+	if(current == 'list_all') {
+		var type = window.location.href.substring(window.location.href.lastIndexOf('type=')+5);
+		$('#subnav_' + type).addClass('active');
+	}
 });
--- TrabalhoPHP-2/templates/common/prod_type_menu.tpl
+++ TrabalhoPHP-2/templates/common/prod_type_menu.tpl
@@ -2,7 +2,7 @@
 	<ul>
 		{if isset($prod_types)}
 			{foreach $prod_types as $prod_type}
-				<li><a href="{$BASE_URL}pages/products/list_all.php?type={$prod_type['type']}">{$prod_type['type']}</a></li>
+				<li id="subnav_{$prod_type['type']}"><a href="{$BASE_URL}pages/products/list_all.php?type={$prod_type['type']}">{$prod_type['type']}</a></li>
 			{/foreach}
 		{/if}
 	</ul>
