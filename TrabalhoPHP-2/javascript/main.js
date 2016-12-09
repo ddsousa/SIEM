13cf13164b3afeba41037b1bea528b1ca2b3d1ee
Pedro Relvas <pedromiguelrelvas@gmail.com>
2016-12-09 10:33:08 +0000

    Page number active style implemented

--- TrabalhoPHP-2/javascript/main.js
+++ TrabalhoPHP-2/javascript/main.js
@@ -3,9 +3,23 @@ $(document).ready(function() {
 	var current = window.location.href.substring(window.location.href.lastIndexOf('/')+1, window.location.href.lastIndexOf('.'));
 	$('#nav_' + current).addClass('active');
 
-	//add subnavbar active class (only on products page)
 	if(current == 'list_all') {
-		var type = window.location.href.substring(window.location.href.lastIndexOf('type=')+5);
-		$('#subnav_' + type).addClass('active');
+		// add subnavbar active class (only on products page)
+		if(window.location.href.indexOf('type=')>0) {
+			var type = window.location.href.substring(window.location.href.lastIndexOf('type=')+5);
+			$('#subnav_' + type).addClass('active');
+		}
+
+		// add page numbers active style (only on products page)
+		if(window.location.href.indexOf('pg=') > 0) {
+			if(window.location.href.indexOf('&') > 0)
+				var page_num = window.location.href.substring(window.location.href.lastIndexOf('pg=')+3, window.location.href.indexOf('&'));
+			else 
+				var page_num = window.location.href.substring(window.location.href.lastIndexOf('pg=')+3);
+		} else {
+			page_num = 1;
+		}
+
+		$('#pg_' + page_num).addClass('active');
 	}
 });
